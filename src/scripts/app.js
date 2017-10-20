8d98df22819aaa208532b560a38f45ee92a7b58e
1013659803 <1013659803@qq.com>
2017-10-20 09:08:49 +0800

    first commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,12 @@
-import '../styles/app.scss';
+//import '../styles/app.scss';
 import Vue from "vue/dist/vue.js";
+
+//vue组件
+import detailHeader from "./pages/details/headerEntry.vue";
+
+new Vue({
+	el : "#details",
+	components : {
+		"detailHeader" : detailHeader
+	}
+})
