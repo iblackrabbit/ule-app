835daec45d67641b87ba75f222c4c47386821bf4
63995881 <63995881@qq.com>
2017-10-23 11:27:03 +0800

    fix config&add router

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,19 @@
-import '../styles/app.scss';
-import Vue from "vue/dist/vue.js";
+import "../styles/app.scss";
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import index from "./pages/index.vue";
+
+import routes from './routes'
+Vue.use(VueRouter);
+const router = new VueRouter({
+    routes,
+    linkActiveClass: 'active'
+})
+
+new Vue({
+    router,
+    components: {
+        index
+    }
+}).$mount('#root');
