67fa9c91f5704bf51ce9737c95d4cc34c91258e5
63995881 <63995881@qq.com>
2017-10-26 15:15:23 +0800

    merge sth

---
+++ src/pages/userself/myUle.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="myUle">
+	<!--<HelloWorld>
+			<router-view></router-view>
+	</HelloWorld>-->
+	<header>
+		<top-info-bar></top-info-bar>
+		<orderlist></orderlist>
+	</header>
+	<center>
+		<my-pocket></my-pocket>
+		<icon-list></icon-list>
+		<div class="fill-blank"></div>
+		<div class="footer"></div>
+	</center>
+
+</div>
+</template>
+
+<script>
+	
+	import VueRouter from 'vue-router';
+	import topInfoBar from './topinfobar.vue';
+	import orderlist from './orderlist.vue';
+	import myPocket from './my-pocket';
+	import iconList from './icon-list';
+	export default{
+	components:{
+		topInfoBar,
+		orderlist,
+		myPocket,
+		iconList
+	}
+}
+	
+</script>
